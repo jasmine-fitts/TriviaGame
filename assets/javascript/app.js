9901e9aa93df7545c7906a8cd4feb3f7d59c9ba2
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-05-17 18:17:52 -0400

    updated index and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,38 @@
+var correctAnswer = ["Giselle", "Houston", "Star", "Lemonade", "false", "Crazy In Love", "Twice", "She was pregnant"]
 
+//Create an array of user guesses 
 
-$(document).ready(function(){
-  $("#start-button").click(function(){
-      
+var userGuesses = []
+
+var wins = 0
+var losses = 0
+
+$(document).ready(function () {
+  $("#start-button").click(function () {
+
+  })
+  $(".ANS").click(function () {
+    var userChoice = $(this).text().trim()
+    userGuesses.push(userChoice)
+
+    console.log(userGuesses);
+
   })
 
-    });
+  $("#submit-button").click(function () {
+    
 
+    for (var i = 0; i < correctAnswer.length; i++) {
+      if (userChoice[i] === correctAnswer[i]) {
+        wins++
+      }
+     else(userChoice[i] !== correctAnswer[i]) 
+        losses++
+
+    }
+
+  });
+
+});
+
+//Create the time interval for the quiz
