0ecedabd443b0202072c7bd32ba425125c8305e0
Jasmine Fitts <jasminefitts@jasmines-mbp.attlocal.net>
2018-05-18 21:10:39 -0400

    changed index, css, and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,21 @@
-var correctAnswer = ["Giselle", "Houston", "Star", "Lemonade", "false", "Crazy In Love", "Twice", "She was pregnant"]
+//List of correct answers to the questions
+var Answers = ["Giselle", "Houston", "Star", "Lemonade", "false", "Crazy In Love", "Twice", "She was pregnant"]
 
 //Create an array of user guesses 
 
 var userGuesses = []
 
-var wins = 0
-var losses = 0
+var correctAnswers = 0
+var incorrectAnswers = 0
 
 $(document).ready(function () {
   $("#start-button").click(function () {
+   
+
 
   })
+  
+  
   $(".ANS").click(function () {
     var userChoice = $(this).text().trim()
     userGuesses.push(userChoice)
@@ -19,16 +24,28 @@ $(document).ready(function () {
 
   })
 
+
+  // Hide the questions until the user click on the start button - jQuery hide method
+  $(".wrapper").click(function() {
+    
+
+  })
+
+  //If the user answers each question within the time frame they are able to submit their choices 
   $("#submit-button").click(function () {
     
 
     for (var i = 0; i < correctAnswer.length; i++) {
       if (userChoice[i] === correctAnswer[i]) {
-        wins++
+        correctAnswers++
       }
      else(userChoice[i] !== correctAnswer[i]) 
-        losses++
+        incorrectAnswers++
 
+    }
+
+    function resetGame(){
+      
     }
 
   });
